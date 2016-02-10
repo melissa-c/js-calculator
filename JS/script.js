72c02b8402e737bb3885a354539cadf8f0bf355b
Lucy Moore <lucyviolet.moore@gmail.com>
2016-02-10 21:59:05 +1300

    clearAll function

--- JS/script.js
+++ JS/script.js
@@ -5,8 +5,10 @@ function clearAll(){
 }
 
 function clearLast(){
-  var clear = document.getElementById('screen').length
-  
+  var clear = document.getElementById('screen').value;
+  clear = clear.substring(0, clear.length-1);
+  document.getElementById('screen').innerHTML = clear;
+
 }
 
 function myFunction(id){
