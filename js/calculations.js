728fc73a366dd82e23556039b38d0eceef0e7b71
louise-r-blue <larblue@rocketmail.com>
2016-02-12 13:01:20 +1300

    Check js file path

--- js/calculations.js
+++ js/calculations.js
@@ -1,33 +1,86 @@
-document.getElementById('one').onclick = getButton();
+window.onload = addCalculator();
 
-document.getElementById('display').value; //GET
-
-document.getElementById('display').innerHTML =//SET
-
-function calculate(e){
-//var e = window.event
-//var btn = e.target || e.srcElement;
-console.log(event.target.id);
-alert (event.target.id);
-//var i = document.getElementById('one').value;
-//document.getElementById('display').innerHTML = 'i';
+function addCalculator () {
+  var el = document.getElementsByTag("button");
+  el.addEventListener("click", getCalculations, false);
 }
+// get all the buttons (children of the parent div)
+function getCalculations () {
+  var btns = document.querySelectorAll("#calculate, button");
+  var operators = ["+", "-", "x", "/", "%"];
+  var decimalUsed = false;
 
-function display(e) {
+// add onclick event to all the buttons and then perform operations
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].onclick = function (e) {
+    // get the input and button values
+      var input = document.querySelctor('.screen');
+      var inputValue = input.innerHTML;
+      var btnValue = this.innerHTML;
+      // append button values to the input string then evaluate
+      // if clear key is presses erase everything
+      if (btnValue === "AC") {
+        input.innerHTML = " ";
+        decimalUsed = false;
+      } else if (btnValue === "=") {    //  if equals button is clicked then calculate and display
+        var calculation = inputValue;
+        var endChar = calculation[calculation.length - 1];
+        // use replace with a regular expression to change operators
+        calculation = calculation.replace(/x/g, "*").replace(/\//g, "/");
+        // then check end character and remove if is  a decimal or operator
+        if (operators.indexOf(endChar) > -1 || endChar === ".") {
+          calculation = calculation.replace(/.$/, " ");
+        }
+        if (calculation) {
+          input.innerHTML = eval(calculation);
+        }
+        decimalUsed = false;
+      } else if (operators.indexOf(btnValue) > -1) {   // do some validation
+        endChar = inputValue[inputValue.length - 1];
+        if (inputValue !==  " " && operators.indexOf(endChar) === -1) {
+          input.innerHTML += btnValue;
+        }
+      // replace last operator with latest btn value clicked
+      // the '.' matches any character and the $ matches end of string
+        if (operators.indexOf(endChar) > -1 && inputValue.length > 1) {
+          input.innerHTML = inputValue.replace(/.$/, btnValue);
+        }
+        decimalUsed = false;
+      } else if (btnValue === ".") {
+        if (!decimalUsed) {
+          input.innerHTML += btnValue;
+          decimalUsed = true;
+        }
+      }
+      else {
+        input.innerHTML += btnValue;
+      }
+    e.preventDefault();
+    };
+  }
+}
+/*function clearAll(){
+  var currentNum = " ";
+  document.calculate.display = currentNum;
+ }
+  
 
-switch(e){
-  case +:
-  document.getElementById('display').innerHTML = '+';
-  break;
-  case x:
-  document.getElementById('display').innerHTML = 'x';
-  break;
-  case /:
-  document.getElementById('display').innerHTML = '/';
-  break;
-  case %:
-  document.getElementById('display').innerHTML = '%';
-  break;
-  default;
-}
-}
+  var operator = document.calculate.operator.value;
+  switch (operator) {
+    case "+":
+      var answer = firstNumber + secondNumber;
+      break;
+    case "-":
+      answer = firstNumber - secondNumber;
+      break;
+    case "x": 
+      answer = firstNumber * secondNumber;
+      break;
+    case "/":
+      answer = firstNumber / secondNumber;
+  }
+  document.calculate.display.value = answer;
+}*/
+
+
+
