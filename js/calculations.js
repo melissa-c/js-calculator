a3736297e5a95064c5118d6b4490ec3099ffb1ce
louise-r-blue <larblue@rocketmail.com>
2016-02-13 15:23:55 +1300

    Check clear last entry

--- js/calculations.js
+++ js/calculations.js
@@ -8,7 +8,7 @@ window.onload = getCalculations(); // this makes sure the html page is fully loa
 }*/
 // get all the buttons (children of the parent div) that we need to operate/calculate with
 function getCalculations () {
-  var btns = document.querySelectorAll("#calculate, button");
+  var btns = document.querySelectorAll("#calculator, button");
   var operators = ["+", "-", "x", "/","%"]; // need modulus % OR is it percent?
   var decimalUsed = false;
 
@@ -25,6 +25,12 @@ function getCalculations () {
       if (btnValue === "AC") { // could put the CE (clear entry) if statment after this if
         input.innerHTML = " ";
         decimalUsed = false;
+        if (btnValue === "CE"){
+        // get the last value on the screen whether numeral, opeartor
+          var clearLast = input.length - 1;
+          input.innerHTML = clearLast;
+          decimalUsed = false;
+        }
       } else if (btnValue === "=") {    //  if equals button is clicked then calculate and display
         var calculation = inputValue;
         var endChar = calculation[calculation.length - 1];
